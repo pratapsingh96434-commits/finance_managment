36f86e29ffb36c9f602151567d77ec3be6efa303
pratapsingh96434-commits <pratapsingh96434@gmail.com>
2025-08-20 12:25:46 +0530

    Delete code.js

--- code.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navToggle = document.querySelector('.nav-toggle');
-    const nav = document.querySelector('.nav');
-
-    navToggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
-});
